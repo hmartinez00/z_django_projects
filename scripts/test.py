98a9790bb58fab1a811903165b2a883abd7b08ea
hmartinez <hmartinez@abae.gob.ve>
2023-05-10 06:38:39 -0400

    Version Estable 20230510 063839

--- scripts/test.py
+++ scripts/test.py
@@ -1,6 +1,7 @@
 from General_Utilities.control_rutas import setting_routes
 from General_Utilities.option_list import option_list
 from modules.django_rootes import *
+import os
 
 
 key = 'resources'
@@ -12,5 +13,12 @@ print(project_path)
 app_list = get_django_apps(project_path)
 print(app_list)
 # app_name = option_list(app_list)
-
 # print(get_project_and_app_py_files(project_path, app_name))
+
+apps_routes = []
+for i in app_list:
+    apps_routes.append(
+        os.path.join(project_path, i)
+    )
+
+print(apps_routes)
