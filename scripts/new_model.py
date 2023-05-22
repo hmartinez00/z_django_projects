04ad8d3664546f51d636fef7cbe7e89df1de3984
hmartinez00 <hectoralonzomartinez00@gmail.com>
2023-05-22 15:39:43 -0400

    Version Estable 20230522 153943

--- scripts/new_model.py
+++ scripts/new_model.py
@@ -1,6 +1,7 @@
 import os
 from modules.django_rootes import *
 from modules.modifile import *
+from General_Utilities.menu import menu_class
 from General_Utilities.control_rutas import setting_routes
 from modules.django_models import ModelGenerator
 
@@ -25,16 +26,18 @@ file_path = ruta_settings[0]
 # Creando el modelo
 model_name = input('Introduzca el nombre del modelo: ')
 
-
 # Crear una instancia de ModelGenerator
 generator = ModelGenerator(file_path, model_name)
-print(generator.model_exists())
-generator.add_model_class()
+menu_class(generator)
 
-# Si no existe, agregar la clase del modelo al archivo
-generator.add_field('id', 'AutoField', ['primary_key=True'])
-generator.add_field('name', 'CharField', ["max_length=100"])
-generator.add_field('age', 'IntegerField')
+
+# print(generator.model_exists())
+# generator.add_model_class()
+
+# # Si no existe, agregar la clase del modelo al archivo
+# generator.add_field('id', 'AutoField', ['primary_key=True'])
+# generator.add_field('name', 'CharField', ["max_length=100"])
+# generator.add_field('age', 'IntegerField')
 
 # generator.add_field()
 # generator.remove_field()
