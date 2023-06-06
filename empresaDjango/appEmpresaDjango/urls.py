72ed02ea5b1af9ab20c004b7c15a17f36a569c69
hmartinez00 <hectoralonzomartinez00@gmail.com>
2023-06-06 13:21:37 -0400

    version estable 20230606 1321

--- empresaDjango/appEmpresaDjango/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-	path('departamento/<int:departamento_id>', views.departamento, name='departamento'),
-	path('', views.index, name='index'),
-]
