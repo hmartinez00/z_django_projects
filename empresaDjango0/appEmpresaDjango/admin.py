bbe8bca9ca8046eda4cbc68f7435c2bbfc828eb6
hmartinez00 <hectoralonzomartinez00@gmail.com>
2023-06-06 13:57:53 -0400

    version estable 20230606 1357

--- empresaDjango0/appEmpresaDjango/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin  
-from .models import *
-
-# Register your models here.
-admin.site.register(Departamento)
-admin.site.register(Habilidad)
-admin.site.register(Empleado)
-
