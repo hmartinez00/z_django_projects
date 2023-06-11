4a20452a9545b9ff352c54026fb5d1d4a644564f
hmartinez <hmartinez@abae.gob.ve>
2023-06-11 12:07:00 -0400

    Version Estable 20230611 120659

--- scripts/new_view.py
+++ scripts/new_view.py
@@ -4,16 +4,16 @@ from modules.django_modelsviews import views
 from General_Utilities.menu import menu_class
 
 
-view_name = 'index'
+# view_name = 'index'
 
 key = 'resources'
 path = setting_routes(key)[0]
 
-projects_list = get_django_projects(path)
-project_path = select_django_project(projects_list)
-apps_list = get_django_apps(project_path)
-app_path = select_django_apps(apps_list)
-app_name = os.path.basename(app_path)
+projects_list   = get_django_projects(path)
+project_path    = select_django_project(projects_list)
+apps_list       = get_django_apps(project_path)
+app_path        = select_django_apps(apps_list)
+app_name        = os.path.basename(app_path)
 
 # Crear una instancia de ModelGenerator
 generator = views(project_path, app_name)
