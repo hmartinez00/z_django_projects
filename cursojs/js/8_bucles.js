91268cbcee5b8b31f04bb9347b67560fbaeaafba
hmartinez <hmartinez@abae.gob.ve>
2023-06-14 17:58:33 -0400

    Version Estable 20230614 175829

---
+++ cursojs/js/8_bucles.js
@@ -0,0 +1,18 @@
+var title = "Bucles";
+document.getElementById("title").innerHTML =title;
+
+// 1. for
+var array = [0,1,2,3,4,5,6];
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    console.log(element)
+}
+
+// 2. while
+let i = 0;
+condition = (i <= 8);
+console.log(condition)
+// while (condition) {
+//     console.log(i);
+//     i++;
+// }
