fcb9b952a749722e6466350a4befb2f35c053ede
hmartinez <hmartinez@abae.gob.ve>
2023-06-14 15:43:26 -0400

    Version Estable 20230614 154323

---
+++ cursojs/js/3_tipos_datos.js
@@ -0,0 +1,13 @@
+var title = "tipos de datos";
+document.getElementById("title").innerHTML =title;
+
+
+// 1. Verificando tipos de variables
+var tipo;
+    // string
+// tipo = 'maria';
+    // number
+// tipo = 123;
+    // boolean
+tipo = true;
+console.log(typeof tipo);
