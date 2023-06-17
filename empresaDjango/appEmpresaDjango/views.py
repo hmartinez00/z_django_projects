e644ae642151d1c070d59f35cda3745db5331655
hmartinez <hmartinez@abae.gob.ve>
2023-06-17 10:10:16 -0400

    Version Estable 20230617 101015

--- empresaDjango/appEmpresaDjango/views.py
+++ empresaDjango/appEmpresaDjango/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Departamento, Empleado
+from .models import Departamento, Empleado, Habilidad
 
 # Create your views here.
 
@@ -13,18 +13,20 @@ def detail(request, departamento_id):
 
 def empleado(request, empleado_id):
     empleado = Empleado.objects.get(pk=empleado_id)
-    return HttpResponse(empleado)
+    context = {
+        'title_template': 'Detalles del empleado',
+        'empleado': empleado,
+    }
+    return render(request, 'empleado.html', context)
 
 def departamentos(request):
     departamentos = Departamento.objects.order_by('nombre') #Aca es donde esta el esquema de modelo-template-views.
     # Se usa el modelo diseñado como un objeto y se interactua con ese objeto en el
     # archivo de vistas. Se explota aqui en este archivo, y luego se arman las respuestas Http.
-    
-    # output = ', '.join([d.nombre for d in departamentos])
-    # return HttpResponse(output)
-    
+        
     context = {
-        'lista_departamentos': departamentos
+        'title_template': 'Listado de departamentos',
+        'lista_departamentos': departamentos,
     }
     return render(request, 'departamento.html', context)
 
