75ce8d9c290dafb202b7d7626a374f1464b0d7a9
hmartinez <hmartinez@abae.gob.ve>
2023-06-17 11:45:46 -0400

    Version Estable 20230617 114545

--- empresaDjango/appEmpresaDjango/urls.py
+++ empresaDjango/appEmpresaDjango/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('departamentos/', views.departamentos, name='departamentos'),
+	path('empleados/', views.empleados, name='empleados'),
 	path('empleado/<int:empleado_id>', views.empleado, name='empleado'),
-	path('detail/<int:departamento_id>', views.detail, name='detail'),
+	path('departamentos/', views.departamentos, name='departamentos'),
+	path('departamento/<int:departamento_id>', views.departamento, name='departamento'),
 	path('', views.index, name='index'),
+	path('test', views.test, name='test'),
 ]
--- empresaDjango/appEmpresaDjango/views.py
+++ empresaDjango/appEmpresaDjango/views.py
@@ -4,20 +4,11 @@ from .models import Departamento, Empleado, Habilidad
 
 # Create your views here.
 
-def index(request):
+def test(request):
     return HttpResponse('Vista simple index.')
 
-def detail(request, departamento_id):
-    departamento = Departamento.objects.get(pk=departamento_id)
-    return HttpResponse(departamento)
-
-def empleado(request, empleado_id):
-    empleado = Empleado.objects.get(pk=empleado_id)
-    context = {
-        'title_template': 'Detalles del empleado',
-        'empleado': empleado,
-    }
-    return render(request, 'empleado.html', context)
+def index(request):
+    return HttpResponse('Vista simple index.')
 
 def departamentos(request):
     departamentos = Departamento.objects.order_by('nombre') #Aca es donde esta el esquema de modelo-template-views.
@@ -28,6 +19,28 @@ def departamentos(request):
         'title_template': 'Listado de departamentos',
         'lista_departamentos': departamentos,
     }
-    return render(request, 'departamento.html', context)
+    return render(request, 'departamentos.html', context)
 
+def departamento(request, departamento_id):
+    departamento = Departamento.objects.get(pk=departamento_id)
+    return HttpResponse(departamento)
+
+def empleados(request):
+    empleados = Empleado.objects.order_by('nombre') #Aca es donde esta el esquema de modelo-template-views.
+    # Se usa el modelo diseñado como un objeto y se interactua con ese objeto en el
+    # archivo de vistas. Se explota aqui en este archivo, y luego se arman las respuestas Http.
+        
+    context = {
+        'title_template': 'Listado de empleados',
+        'lista_empleados': empleados,
+    }
+    return render(request, 'empleados.html', context)
+
+def empleado(request, empleado_id):
+    empleado = Empleado.objects.get(pk=empleado_id)
+    context = {
+        'title_template': 'Detalles del empleado',
+        'empleado': empleado,
+    }
+    return render(request, 'empleado.html', context)
 
