c87d24f9671c4b4c4646c2ccced0cdc9e22799d4
hmartinez <hmartinez@abae.gob.ve>
2023-06-19 19:32:35 -0400

    Version Estable 20230619 193234

--- CursoBsw3shools/BootStrap5/models.py
+++ CursoBsw3shools/BootStrap5/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+
+class Tutorial_de_Bootstrap_5(models.Model):
+    content = models.CharField(max_length=100)
+
+    def __str__(self):
+        out = f'content={self.content} '
+        return out
+
+class Bootstrap_5_formularios(models.Model):
+    content = models.CharField(max_length=100)
+
+    def __str__(self):
+        out = f'content={self.content} '
+        return out
+
+class Rejilla_Bootstrap_5(models.Model):
+    content = models.CharField(max_length=100)
+
+    def __str__(self):
+        out = f'content={self.content} '
+        return out
+
+class Bootstrap_5_Otros(models.Model):
+    content = models.CharField(max_length=100)
+
+    def __str__(self):
+        out = f'content={self.content} '
+        return out
+
+class Certificacion(models.Model):
+    content = models.CharField(max_length=100)
+
+    def __str__(self):
+        out = f'content={self.content} '
+        return out
