20387a67eb9b1e79ba32c0dd3af952247fdb6ea4
hmartinez <hmartinez@abae.gob.ve>
2023-06-19 15:21:48 -0400

    version estable 20230619 1521

--- CursoBsw3shools/BootStrap5/urls.py
+++ CursoBsw3shools/BootStrap5/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+	path('test/', views.test, name='test'),
+	path('', views.index, name='index'),
 ]
--- CursoBsw3shools/BootStrap5/views.py
+++ CursoBsw3shools/BootStrap5/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def test(request):
+    # modelos = ', '.join(apps.get_models())
+    return HttpResponse('Vista de Prueba!')
+
+def index(request):
+    clases = ['Clase1', 'Clase2']
+    context = {
+        'view_name': 'inicio',
+        'title_template': 'Listado de Clases',
+        'lista_clases': clases,
+    }
+    return render(request, 'index.html', context)
