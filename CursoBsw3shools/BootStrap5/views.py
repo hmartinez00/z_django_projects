bb172a7b89831f8afa000e3bea35f41cb617f159
hmartinez <hmartinez@abae.gob.ve>
2023-06-19 16:10:48 -0400

    Version Estable 20230619 161047

--- CursoBsw3shools/BootStrap5/views.py
+++ CursoBsw3shools/BootStrap5/views.py
@@ -8,7 +8,39 @@ def test(request):
     return HttpResponse('Vista de Prueba!')
 
 def index(request):
-    clases = ['Clase1', 'Clase2']
+    clases = [
+        'BS5_INICIO',
+        'BS5_Empezar',
+        'Contenedores_BS5',
+        'BS5_Rejilla_Básica',
+        'Tipografía_BS5',
+        'Colores_BS5',
+        'Tablas_BS5',
+        'Imágenes_BS5',
+        'Jumbotrón_BS5',
+        'Alertas_BS5',
+        'Botones_BS5',
+        'Grupos_de_botones_BS5',
+        'Insignias_BS5',
+        'Barras_de_progreso_BS5',
+        'Hilanderos_BS5',
+        'Paginación_BS5',
+        'Grupos_de_listas_BS5',
+        'Tarjetas_BS5',
+        'Menús_desplegables_BS5',
+        'BS5_Colapso',
+        'Navegadores_BS5',
+        'Barra_de_navegación_BS5',
+        'Carrusel_BS5',
+        'Modalidad_BS5',
+        'Información_sobre_herramientas_BS5',
+        'Ventana_emergente_BS5',
+        'Tostadas_BS5',
+        'BS5_Scrollspy',
+        'BS5_Offcanvas',
+        'Utilidades_BS5',
+        'Flexibilidad_BS5',
+    ]
     context = {
         'view_name': 'inicio',
         'title_template': 'Listado de Clases',
