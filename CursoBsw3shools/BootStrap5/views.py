ad6c079e23d0e5ef389e5add57abe438bf4cf1b2
hmartinez <hmartinez@abae.gob.ve>
2023-06-20 09:50:39 -0400

    Version Estable 20230620 095038

--- CursoBsw3shools/BootStrap5/views.py
+++ CursoBsw3shools/BootStrap5/views.py
@@ -10,20 +10,16 @@ def test(request):
     return HttpResponse('Vista de Prueba!')
 
 def index(request):
-    clases_Tutorial_de_Bootstrap_5 = [str(i).split(',')[0].split('=')[1] for i in list(Tutorial_de_Bootstrap_5.objects.order_by('id'))]
-    Modulos = {
-        'Tutorial_de_Bootstrap_5': [str(i).split(',')[0].split('=')[1] for i in list(Tutorial_de_Bootstrap_5.objects.order_by('id'))],
-        'Bootstrap_5_formularios': [str(i).split(',')[0].split('=')[1] for i in list(Tutorial_de_Bootstrap_5.objects.order_by('id'))],
-        'Rejilla_Bootstrap_5': [str(i).split(',')[0].split('=')[1] for i in list(Tutorial_de_Bootstrap_5.objects.order_by('id'))],
-        'Bootstrap_5_Otros': [str(i).split(',')[0].split('=')[1] for i in list(Tutorial_de_Bootstrap_5.objects.order_by('id'))],
-        'Certificacion': [str(i).split(',')[0].split('=')[1] for i in list(Tutorial_de_Bootstrap_5.objects.order_by('id'))],
+    modules = {
+        'Tutorial_de_Bootstrap_5'   : [str(i).split(',')[0].split('=')[1] for i in list(Tutorial_de_Bootstrap_5.objects.order_by('id')) ],
+        'Bootstrap_5_formularios'   : [str(i).split(',')[0].split('=')[1] for i in list(Bootstrap_5_formularios.objects.order_by('id')) ],
+        'Rejilla_Bootstrap_5'       : [str(i).split(',')[0].split('=')[1] for i in list(Rejilla_Bootstrap_5.objects.order_by('id'))     ],
+        'Bootstrap_5_Otros'         : [str(i).split(',')[0].split('=')[1] for i in list(Bootstrap_5_Otros.objects.order_by('id'))       ],
+        'Certificacion'             : [str(i).split(',')[0].split('=')[1] for i in list(Certificacion.objects.order_by('id'))           ],
     }
-    module_name = list(Modulos.keys())[0]
-    class_list = Modulos[module_name]
-    context = {
-        'view_name': 'inicio',
+    context     = {
+        'view_name'     : 'inicio',
         'title_template': 'Listado de Clases',
-        'module_name': module_name,
-        'class_list': class_list,
+        'modules'       : modules,
     }
     return render(request, 'index.html', context)
