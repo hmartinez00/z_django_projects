b2f9d3229c8052aae8c9408156b00c2f6a8cd067
hmartinez <hmartinez@abae.gob.ve>
2023-06-20 08:36:10 -0400

    Version Estable 20230620 083609

--- CursoBsw3shools/BootStrap5/views.py
+++ CursoBsw3shools/BootStrap5/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Tutorial_de_Bootstrap_5, Bootstrap_5_formularios, Rejilla_Bootstrap_5, Bootstrap_5_Otros, Certificacion
+
 
 # Create your views here.
 
@@ -8,42 +10,20 @@ def test(request):
     return HttpResponse('Vista de Prueba!')
 
 def index(request):
-    clases = [
-        'BS5_INICIO',
-        'BS5_Empezar',
-        'Contenedores_BS5',
-        'BS5_Rejilla_Básica',
-        'Tipografía_BS5',
-        'Colores_BS5',
-        'Tablas_BS5',
-        'Imágenes_BS5',
-        'Jumbotrón_BS5',
-        'Alertas_BS5',
-        'Botones_BS5',
-        'Grupos_de_botones_BS5',
-        'Insignias_BS5',
-        'Barras_de_progreso_BS5',
-        'Hilanderos_BS5',
-        'Paginación_BS5',
-        'Grupos_de_listas_BS5',
-        'Tarjetas_BS5',
-        'Menús_desplegables_BS5',
-        'BS5_Colapso',
-        'Navegadores_BS5',
-        'Barra_de_navegación_BS5',
-        'Carrusel_BS5',
-        'Modalidad_BS5',
-        'Información_sobre_herramientas_BS5',
-        'Ventana_emergente_BS5',
-        'Tostadas_BS5',
-        'BS5_Scrollspy',
-        'BS5_Offcanvas',
-        'Utilidades_BS5',
-        'Flexibilidad_BS5',
-    ]
+    clases_Tutorial_de_Bootstrap_5 = [str(i).split(',')[0].split('=')[1] for i in list(Tutorial_de_Bootstrap_5.objects.order_by('id'))]
+    Modulos = {
+        'Tutorial_de_Bootstrap_5': [str(i).split(',')[0].split('=')[1] for i in list(Tutorial_de_Bootstrap_5.objects.order_by('id'))],
+        'Bootstrap_5_formularios': [str(i).split(',')[0].split('=')[1] for i in list(Tutorial_de_Bootstrap_5.objects.order_by('id'))],
+        'Rejilla_Bootstrap_5': [str(i).split(',')[0].split('=')[1] for i in list(Tutorial_de_Bootstrap_5.objects.order_by('id'))],
+        'Bootstrap_5_Otros': [str(i).split(',')[0].split('=')[1] for i in list(Tutorial_de_Bootstrap_5.objects.order_by('id'))],
+        'Certificacion': [str(i).split(',')[0].split('=')[1] for i in list(Tutorial_de_Bootstrap_5.objects.order_by('id'))],
+    }
+    module_name = list(Modulos.keys())[0]
+    class_list = Modulos[module_name]
     context = {
         'view_name': 'inicio',
         'title_template': 'Listado de Clases',
-        'lista_clases': clases,
+        'module_name': module_name,
+        'class_list': class_list,
     }
     return render(request, 'index.html', context)
--- scripts/test2.py
+++ scripts/test2.py
@@ -1,52 +1,10 @@
 import os
-import json
-from ManageDB.sqlite_on_db import *
-import sys
-import sqlite3
-
-sys.stdout.reconfigure(encoding='utf-8')
 
 
-database = r"C:\Users\admin\Documents\0 - A Repositorios GitHub\z_django_projects\CursoBsw3shools\db.sqlite3"
-ruta_archivo_json = r'C:\Users\admin\Documents\0 - A Repositorios GitHub\z_django_projects\CursoBsw3shools\static\estructura_curso.json'
+diccionario = {
+    'clave1': 1,
+    'clave2': 1,
+    'clave3': 1,
+}
 
-with open(ruta_archivo_json, encoding='utf-8') as archivo_json:
-    datos_json = json.load(archivo_json)
-
-
-tables = list(datos_json.keys())
-table = 'BootStrap5_' + tables[0]
-# BootStrap5_tutorial_de_bootstrap_5
-print(len(tables))
-values = datos_json[tables[0]]
-
-
-for i in range(len(values)):
-    renglon = (i,values[i])
-    print(renglon)
-
-    insert(database, table, renglon)
-
-
-
-
-# # Establecer la conexión con la base de datos
-# conn = sqlite3.connect(database)
-
-# # Crear un cursor para ejecutar consultas
-# cursor = conn.cursor()
-
-# # Ejecutar una consulta
-# cursor.execute(f'SELECT * FROM {table}')
-
-# # Obtener los resultados de la consulta
-# results = cursor.fetchall()
-
-# # Recorrer los resultados
-# for row in results:
-#     print(row)
-
-# # Cerrar el cursor y la conexión
-# cursor.close()
-# conn.close()
-
+print(list(diccionario.keys())[0])
