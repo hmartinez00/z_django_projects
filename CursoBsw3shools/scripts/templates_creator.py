04eae82080c7ca2321a9624144816953aa30c35a
hmartinez <hmartinez@abae.gob.ve>
2023-06-21 16:24:29 -0400

    Version Estable 20230621 162428

--- CursoBsw3shools/scripts/templates_creator.py
+++ CursoBsw3shools/scripts/templates_creator.py
@@ -1,33 +1,32 @@
-import os
-import json
-from ManageDB.sqlite_on_db import *
 import sys
-from unidecode import unidecode
-import sqlite3
+import unicodedata
 
-sys.stdout.reconfigure(encoding='utf-8')
+def remove_accents(value):
+    normalized_value = unicodedata.normalize('NFKD', value)
+    ascii_value = normalized_value.encode('ASCII', 'ignore').decode('utf-8')
+    return ascii_value
 
 def replace_spaces(value):
-    new_value = value\
+    new_value = remove_accents(value)\
         .replace(' ', '_')\
-        .replace('á', 'a')\
-        .replace('é', 'e')\
-        .replace('í', 'i')\
-        .replace('ó', 'o')\
-        .replace('ú', 'u').lower()
+        .lower()
         
     return new_value
 
 
-database = r"CursoBsw3shools\db.sqlite3"
-ruta_archivo_json = r'CursoBsw3shools\static\estructura_curso.json'
+s = replace_spaces('ácento')
+print(s)
 
-with open(ruta_archivo_json, encoding='utf-8') as archivo_json:
-    datos_json = json.load(archivo_json)
 
-tables = list(datos_json.keys())
-values = []
-for i in datos_json[tables[0]]:
-    values.append(replace_spaces(i))
+# database = r"CursoBsw3shools\db.sqlite3"
+# ruta_archivo_json = r'CursoBsw3shools\static\estructura_curso.json'
 
-print(tables, values)
+# with open(ruta_archivo_json, encoding='utf-8') as archivo_json:
+#     datos_json = json.load(archivo_json)
+
+# tables = list(datos_json.keys())
+# values = []
+# for i in datos_json[tables[0]]:
+#     values.append(replace_spaces(i))
+
+# print(tables, values)
