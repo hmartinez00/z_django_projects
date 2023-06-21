1be4610f9432d8bd0c68b2c46cc1f6aebb43b9b3
hmartinez <hmartinez@abae.gob.ve>
2023-06-21 15:51:42 -0400

    Version Estable 20230621 155141

--- CursoBsw3shools/BootStrap5/templatetags/custom_filters.py
+++ CursoBsw3shools/BootStrap5/templatetags/custom_filters.py
@@ -4,4 +4,12 @@ register = template.Library()
 
 @register.filter
 def replace_spaces(value):
-    return value.replace(' ', '_')
+    new_value = value\
+        .replace(' ', '_')\
+        .replace('á', 'a')\
+        .replace('é', 'e')\
+        .replace('í', 'i')\
+        .replace('ó', 'o')\
+        .replace('ú', 'u')
+        
+    return new_value
