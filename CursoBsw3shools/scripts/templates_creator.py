9498f2bfda2f4452160f0ee633c77bcefc61b6e4
hmartinez <hmartinez@abae.gob.ve>
2023-06-22 10:58:21 -0400

    Version Estable 20230622 105820

--- CursoBsw3shools/scripts/templates_creator.py
+++ CursoBsw3shools/scripts/templates_creator.py
@@ -1,45 +1,22 @@
-import os
-import sys
-import json
+from General_Utilities.option_list import option_list
+from ManageDB.sqlite_on_db import selectall_id, show_tables
+import unidecode
 
 
-sys.stdout.reconfigure(encoding='utf-8')
+database = r'CursoBsw3shools\db.sqlite3'
+db_tables = show_tables(database)
 
-def normalizar(value):
-    str_string = str(str(value).lower())
-    replacements = {
-        ' ': '_',
-        'á': 'a',
-        'é': 'e',
-        'í': 'i',
-        'ó': 'o',
-        'ú': 'u',
-    }
-    new_value = ''
-    for char in str_string:
-        if char in replacements:
-            new_value += replacements[char]
-        else:
-            new_value += char
-    return new_value
+tables = []
+for i in db_tables:
+    if 'BootStrap5_' in i:
+        tables.append(i)
 
-directory = os.getcwd()
+tabla = option_list(tables)
 
-dir = 'CursoBsw3shools'
-os.chdir(dir)
-print(os.getcwd())
+id = 'id'
+df = selectall_id(database, tabla, id)['content']
+lista = []
+for i in df:
+    lista.append(unidecode.unidecode(i).replace(' ', '_').lower())
 
-database = r"db.sqlite3"
-ruta_archivo_json = r'static\estructura_curso.json'
-
-with open(ruta_archivo_json, encoding='utf-8') as archivo_json:
-    datos_json = json.load(archivo_json)
-
-tables = list(dict(datos_json).keys())
-values = datos_json[tables[0]]
-
-for i in values:
-    print(normalizar(i))
-
-
-os.chdir(directory)
+print(lista)
