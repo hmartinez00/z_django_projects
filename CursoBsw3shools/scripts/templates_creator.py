8a9a525eb3a8fe60dedb5536a37bb5bb93de79c7
hmartinez <hmartinez@abae.gob.ve>
2023-06-22 15:51:40 -0400

    Version Estable 20230622 155139

--- CursoBsw3shools/scripts/templates_creator.py
+++ CursoBsw3shools/scripts/templates_creator.py
@@ -1,9 +1,21 @@
+import os
 from General_Utilities.option_list import option_list
 from ManageDB.sqlite_on_db import selectall_id, show_tables
 import unidecode
+from modules.django_rootes import *
+from General_Utilities.control_rutas import setting_routes
 
 
-database = r'CursoBsw3shools\db.sqlite3'
+key = 'resources'
+path = setting_routes(key)[0]
+
+projects_list   = get_django_projects(path)
+project_path    = select_django_project(projects_list)
+apps_list       = get_django_apps(project_path)
+app_path        = select_django_apps(apps_list)
+app_name        = os.path.basename(app_path)
+
+database = os.path.join(project_path, 'db.sqlite3')
 db_tables = show_tables(database)
 
 tables = []
@@ -20,3 +32,4 @@ for i in df:
     lista.append(unidecode.unidecode(i).replace(' ', '_').lower())
 
 print(lista)
+
