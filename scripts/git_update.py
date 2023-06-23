60b179d17a08301eca63019b0745749a723ccf32
hmartinez00 <hectoralonzomartinez00@gmail.com>
2023-06-23 11:53:19 -0400

    Version Estable 20230623 115318

--- scripts/git_update.py
+++ scripts/git_update.py
@@ -5,19 +5,20 @@ from General_Utilities.option_list import option_list
 from Pku_module.Package_update_module import listar_paquetes
 from modules.django_rootes import *
 
-directory = os.getcwd()
+# directory = os.getcwd()
 
-key = 'resources'
-path = setting_routes(key)[0]
+# key = 'resources'
+# path = setting_routes(key)[0]
 
-projects_list   = get_django_projects(path)
-project_path    = select_django_project(projects_list)
+# projects_list   = get_django_projects(path)
+# project_path    = select_django_project(projects_list)
 # apps_list       = get_django_apps(project_path)
 # app_path        = select_django_apps(apps_list)
 
-os.chdir(project_path)
-opciones = os.listdir()
-project = option_list(opciones)
-auto_commit(project)
+# os.chdir(project_path)
+# opciones = os.listdir()
+# project = option_list(opciones)
+# auto_commit(project)
+auto_commit()
 
-os.chdir(directory)
+# os.chdir(directory)
