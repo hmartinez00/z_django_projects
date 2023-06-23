7e73027d84ec1999c1f76342d37a4930caa6815d
hmartinez00 <hectoralonzomartinez00@gmail.com>
2023-06-23 11:38:50 -0400

    version estable 20230623 1138

--- scripts/git_update.py
+++ scripts/git_update.py
@@ -12,8 +12,8 @@ path = setting_routes(key)[0]
 
 projects_list   = get_django_projects(path)
 project_path    = select_django_project(projects_list)
-apps_list       = get_django_apps(project_path)
-app_path        = select_django_apps(apps_list)
+# apps_list       = get_django_apps(project_path)
+# app_path        = select_django_apps(apps_list)
 
 os.chdir(project_path)
 opciones = os.listdir()
