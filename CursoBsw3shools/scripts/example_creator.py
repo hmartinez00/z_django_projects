e0ac57e4d3bc43e598d7a14fa36edf5e6d8f8eb1
hmartinez <hmartinez@abae.gob.ve>
2023-06-23 15:56:00 -0400

    Version Estable 20230623 155559

---
+++ CursoBsw3shools/scripts/example_creator.py
@@ -0,0 +1,19 @@
+import os
+
+# Introducimos la ruta
+final_dir = input('Introduzca la ruta de destino: ')
+
+# Nombramos el archivo a crear
+index = len(os.listdir(final_dir))
+file_name = os.path.join(final_dir, f'example{index}.html')
+
+# Creamos el archivo con el contenido
+if not os.path.exists(file_name):
+    with open(file_name, 'w') as file:
+        content = '''{% extends "base.html" %}
+        {% block content %}
+
+        {% endblock %}
+        '''
+        file.write(content)
+
