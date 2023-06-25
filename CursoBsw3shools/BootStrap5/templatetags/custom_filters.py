bd7a0b9075f8a62384261fc1cb9b1c0a49bbd30e
hmartinez <hmartinez@abae.gob.ve>
2023-06-24 20:12:11 -0400

    Version Estable 20230624 201210

--- CursoBsw3shools/BootStrap5/templatetags/custom_filters.py
+++ CursoBsw3shools/BootStrap5/templatetags/custom_filters.py
@@ -5,12 +5,17 @@ import re
 register = template.Library()
 
 @register.filter
-def replace_spaces(string):
+def replace_signs(string):
     new_string = unidecode.unidecode(string).lower()
-    return new_string.replace(' ', '_')
+    return str(new_string).replace(' ', '_')
 
 @register.filter
 def extract_numbers(string):
     numbers = re.findall(r'\d+', string)
     numbers = int(numbers[0])
     return numbers
+
+@register.filter
+def replace_space(string):
+    new_string = str(string).replace('_', ' ')
+    return new_string
