e3d6ca05d9bbe00d1c761b8a53b0a246f8b537ed
hmartinez <hmartinez@abae.gob.ve>
2023-06-26 18:01:10 -0400

    Version Estable 20230626 180110

--- scripts/test2.py
+++ scripts/test2.py
@@ -130,7 +130,7 @@ team = input('Introduzca su equipo favorito: ')
 
     # 3. Validamos
 for i in LVBP:
-    if team.lower() in i.lower():
+    if team.lower()==i.lower():
         print(f'Ha seleccionado al glorioso equipo de: {i}!')
         encontrado = True
         break
@@ -145,9 +145,77 @@ print('''
 Ejercicio 3: De donde vienes?
 --------------------------------------------------''')
 
+gusto_playa = input("Te gusta la playa? (S/N): ")
+gusto_chinchorro = input("Te gusta ir a los chinchorros? (S/N): ")
+gusto_rios = input("Te gusta visitar los rios? (S/N): ")
+
+es_oriental = False
+
+while not es_oriental:
+    if gusto_playa.lower() == "s" or gusto_chinchorro.lower() == "s" or gusto_rios.lower() == "s":
+        print("Usted parece oriental.")
+        procedencia = input("Indique su procedencia: ")
+        if procedencia.lower() == "nacido" or procedencia.lower() == "criado":
+            print("A usted le gusta el oriente!")
+            break
+    else:
+        print("Respuestas:")
+        print("Gusto playa:", gusto_playa)
+        print("Gusto chinchorro:", gusto_chinchorro)
+        print("Gusto rios:", gusto_rios)
+        break
 
 
 
+# Ejercicio 4: 
+print('''
+Ejercicio 4: Generadores
+--------------------------------------------------''')
+
+def generador_capitales():
+    capitales = []
+    while True:
+        capital = input("Ingrese una capital de estado de Venezuela (o 'salir' para terminar): ")
+        if capital.lower() == "salir":
+            break
+        capitales.append(capital)
+        yield capital
+
+generador = generador_capitales()
+
+print("Las primeras 10 capitales ingresadas son:")
+for _ in range(10):
+    try:
+        capital = next(generador)
+        print(capital)
+    except StopIteration:
+        print("Se han ingresado menos de 10 capitales.")
+        break
+
+
+# Ejercicio 5: 
+print('''
+Ejercicio 5: Generadores
+--------------------------------------------------''')
+
+def generador_nombres(nombre1, nombre2, nombre3):
+    nombres = [nombre1, nombre2, nombre3]
+
+    for nombre in nombres:
+        if len(nombre) >= 7:
+            yield nombre[:8]
+
+# Solicitar los nombres al usuario
+nombre1 = input("Ingrese el primer nombre: ")
+nombre2 = input("Ingrese el segundo nombre: ")
+nombre3 = input("Ingrese el tercer nombre: ")
+
+# Crear una instancia del generador
+gen = generador_nombres(nombre1, nombre2, nombre3)
+
+# Imprimir los primeros ocho caracteres de los nombres que cumplen con el requisito
+for nombre in gen:
+    print(nombre)
 
 
 
