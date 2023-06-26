0a2bf66d0471f77108970c8c61b64f3ee0be3503
hmartinez00 <hectoralonzomartinez00@gmail.com>
2023-06-26 13:12:32 -0400

    Version Estable 20230626 131232

--- scripts/test2.py
+++ scripts/test2.py
@@ -20,22 +20,15 @@ user_dates = {
 for i in user_dates:
     user_dates[i] = input(f'Por favor introduzca su {i}: '.replace('_', ' '))
 
-validate_condition = \
-    (int(user_dates['edad']) >= 18  ) and \
-    (int(user_dates['edad']) <  120 )
-
 condition = \
     (int(user_dates['numero_de_hijos']) > 5     ) and \
     (int(user_dates['edad'])            > 60    ) and \
     (float(user_dates['sueldo'])        < 2500  )
 
-if validate_condition:
-    if condition:
-        print('\n\nExento de pago ISLR')
-    else:
-        print('\n\nDebe realizar las gestiones para el pago de su ISLR ante el SENIAT')
+if condition:
+    print('\n\nExento de pago ISLR')
 else:
-    print('\n\nPor favor verifique la edad del usuario.')
+    print('\n\nDebe realizar las gestiones para el pago de su ISLR ante el SENIAT')
 
 
 # EOF
